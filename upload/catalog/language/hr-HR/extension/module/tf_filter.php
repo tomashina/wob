8c7f98e456057d0abaf1912d3c6002f6a7347cb3
tomashina <tomislav@agmedia.hr>
2026-08-11 21:11:00 +0200

    Polish product UI and unify Font Awesome icons

---
+++ upload/catalog/language/hr-HR/extension/module/tf_filter.php
@@ -0,0 +1,20 @@
+<?php
+$_['heading_title'] = 'Filter';
+
+$_['text_price'] = 'Cijena';
+$_['text_sub_category'] = 'Kategorija';
+$_['text_manufacturer'] = 'Brand';
+$_['text_search'] = 'Pretraži';
+$_['text_search_placeholder'] = 'Pretraži';
+$_['text_availability'] = 'Dostupnost';
+$_['text_discount'] = 'Popust';
+$_['text_discount_rate'] = '%d%% popusta ili više';
+$_['text_rating'] = 'Ocjena';
+$_['text_in_stock'] = 'Dostupno';
+$_['text_out_of_stock'] = 'Nije dostupno';
+$_['text_and_up'] = 'i više';
+$_['text_no_result'] = 'Nema pronađenih proizvoda.';
+$_['text_reset'] = 'Očisti';
+$_['text_reset_all'] = 'Očisti sve';
+$_['text_see_more'] = 'Prikaži više';
+$_['text_see_less'] = 'Prikaži manje';
